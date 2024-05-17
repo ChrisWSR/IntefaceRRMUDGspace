4d3373f653c33000919d84764f369cd3918359d1
chris <wilcris7321@gmail.com>
2024-05-16 22:17:51 -0600

    hammer and bottle model

--- detection.py
+++ detection.py
@@ -21,7 +21,7 @@ class Detection(QThread):
 
     def __init__(self):
         super().__init__()
-        self.yolo_model = YOLO("best.pt")
+        self.yolo_model = YOLO("hammer_bottle.pt")
         self.video_capture = cv2.VideoCapture(0)
         self.video_capture.set(3, 640)
         self.video_capture.set(4, 640)
